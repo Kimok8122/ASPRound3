73516845b9a2eb08c336aaa5fcf03dab3d66f476
KimoK8122 <kimo.k@me.com>
2020-09-30 13:27:21 -0600

    View one product at a time

---
+++ ASPRound3/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ASPRound3.Models;
+
+namespace ASPRound3
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+
+
+
+    }
+}
---
+++ ASPRound3/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using ASPRound3.Models;
+using Dapper;
+
+namespace ASPRound3
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+
+    }
+}
